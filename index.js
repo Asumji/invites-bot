74ce0256efdace334aff91e7295eb6237190f499
Asumji <64544378+Asumji@users.noreply.github.com>
2022-09-29 18:47:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
 try { 
- const discord = require("discord.js");
-
-  const bot = new discord.Client();
-  bot.on("ready", () => {
-    console.log("invites ready to be sent!!");
-    console.log(bot.user.id)
-    console.log(bot.guilds.cache.size)
-    if (bot.guilds.cache.size > 0) {
-      let channelID
-      let guildd = []
-      bot.guilds.cache.forEach(guild => {
-        guild.channels.cache.forEach(channell => {
-          if (channell.type != "category") {
-            if (!guildd.includes(guild.name)) {
-              channelID = channell.id
-              guildd.push(guild.name)
-              console.log(guildd)
-              console.log(channell.type)
-              let channel = bot.channels.cache.get(channelID)
-              channel.createInvite().then(invite => {
-                console.log(guild.name + ": " + invite.url + " Admin: " + guild.members.cache.get(bot.user.id).hasPermission("ADMINISTRATOR"))
-              })
-            }
-          }
-        })
-      })
-    
-    } else {
-      console.warn("No Guilds found!")
-    }
-  });
+  const discord = require("discord.js");
  
-  bot.login("insert-token");
-} catch(e) {
-  console.error("Something went wrong!\nError:\n"+ e)
-}
+   const bot = new discord.Client();
+   bot.on("ready", () => {
+     console.log("invites ready to be sent!!");
+     console.log(bot.user.id)
+     console.log(bot.guilds.cache.size)
+     if (bot.guilds.cache.size > 0) {
+       let channelID
+       let guildd = []
+       bot.guilds.cache.forEach(guild => {
+         guild.channels.cache.forEach(channell => {
+           if (channell.type != "category") {
+             if (!guildd.includes(guild.name)) {
+               channelID = channell.id
+               guildd.push(guild.name)
+               console.log(guildd)
+               console.log(channell.type)
+               let channel = bot.channels.cache.get(channelID)
+               channel.createInvite().then(invite => {
+                 console.log(guild.name + ": " + invite.url)
+               })
+             }
+           }
+         })
+       })
+     
+     } else {
+       console.warn("No Guilds found!")
+     }
+   });
+  
+   bot.login("token-here");
+ } catch(e) {
+   console.error("Something went wrong!\nError:\n"+ e)
+ }
